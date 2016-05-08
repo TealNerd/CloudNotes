15f70e993108445fc7be79216cd5ccda04d72852
Stephen Rubio <stephenrubio94@gmail.com>
2016-05-08 17:04:47 -0400

    Fixed path issues, added basic GUI.

--- src/main/java/com/biggestnerd/cloudnotes/CloudNotes.java
+++ src/main/java/com/biggestnerd/cloudnotes/CloudNotes.java
@@ -1,5 +1,31 @@
 package com.biggestnerd.cloudnotes;
 
-public class CloudNotes {
-//TODO: write some frickin code
+//TODO: Everything
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class CloudNotes extends JFrame
+{
+    private JPanel panel;
+    private JOptionPane pane = new JOptionPane();
+    private JButton exitButton;
+    
+    public CloudNotes()
+    {
+    	super("Cloud Notes");
+    	
+    	setSize(700, 330);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel();
+        
+        //Code goes here
+        
+        add(panel);
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) { CloudNotes gui = new CloudNotes(); }
+
 }
