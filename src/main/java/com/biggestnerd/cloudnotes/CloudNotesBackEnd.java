415d09df546e8a574663e3c244829b3fbc3b26b1
Stephen Rubio <stephenrubio94@gmail.com>
2016-05-08 19:36:02 -0400

    File read/writing works

--- src/main/java/com/biggestnerd/cloudnotes/CloudNotesBackEnd.java
+++ src/main/java/com/biggestnerd/cloudnotes/CloudNotesBackEnd.java
@@ -1,15 +1,40 @@
 package com.biggestnerd.cloudnotes;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class CloudNotesBackEnd
 {
-	public void sync(String input)
+	public String filename;
+	
+	public void sync(String input) throws IOException
 	{
-		System.out.println("Syncing: " + input);
+		System.out.println("Syncing: " + input);	
+		
+	    PrintWriter pWriter = new PrintWriter (new FileWriter (new File (filename)));
+	    pWriter.println(input);
+	    pWriter.close();
 	}
 	
-	public String load()
+	public String load()throws Exception 
 	{
 		System.out.println("Loading");
-		return "Test text";
+
+		String input = "";
+				
+	    Scanner sc = new Scanner (new File (filename));
+	    while (sc.hasNextLine())
+	    	input += sc.nextLine() + "\n";
+	    sc.close();
+		
+		return input;
+	}
+	
+	public void setFileName(String newFileName)
+	{
+		filename = newFileName;
 	}
 }
--- src/main/java/com/biggestnerd/cloudnotes/CloudNotesGUI.java
+++ src/main/java/com/biggestnerd/cloudnotes/CloudNotesGUI.java
@@ -3,6 +3,7 @@ package com.biggestnerd.cloudnotes;
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.event.*;
+import java.io.File;
 
 public class CloudNotesGUI extends JFrame
 {
@@ -11,23 +12,25 @@ public class CloudNotesGUI extends JFrame
     private JButton syncButton;
     private JButton loadButton;
     
-    CloudNotesBackEnd backEnd = new CloudNotesBackEnd();
+    CloudNotesBackEnd backEnd;
     
     public CloudNotesGUI()
     {
     	super("Cloud Notes");
+    	backEnd = new CloudNotesBackEnd();
+    	
+        backEnd.setFileName((new File(new File(System.getProperty("user.home"), "/Desktop"), "test.txt")).toString());
     	
         if (System.getProperty("os.name").startsWith("Mac OS"))
         	setSize(350, 390);
         else 
         	setSize(350, 390);
         
-        addWindowListener(new WindowAdapter() { public void windowClosing(WindowEvent e) { onExit(); }});
-        
+        addWindowListener(new WindowAdapter() { public void windowClosing(WindowEvent e) { try { onExit(); } catch (Exception e1) { e1.printStackTrace(); } }});
+                
         panel = new JPanel();
         panel.setLayout(new BorderLayout());
         
-        //Code goes here
         syncButton = new JButton("Sync");
         syncButton.addActionListener(new syncListener());
         
@@ -41,34 +44,43 @@ public class CloudNotesGUI extends JFrame
         subPanel.add(syncButton);
         subPanel.add(loadButton);
         
-        panel.add(subPanel,  BorderLayout.NORTH);
+        panel.add(subPanel, BorderLayout.NORTH);
         panel.add(notepad, BorderLayout.CENTER);
         
         add(panel);
-        setVisible(true);
+        setVisible(true);        
     }
     
-    public void sync()
+    public void sync() throws Exception
     {
     	backEnd.sync(notepad.getText());
     }
     
-    public void load()
+    public void load() throws Exception
     {
     	notepad.setText(backEnd.load());
     }
     
-    private void onExit()
+    private void onExit() throws Exception
     {
+    	//Check if test has been edited before asking to sync again?
     	System.out.println("Closing");
         if (JOptionPane.showConfirmDialog(null, "Sync Before Exiting?", "Exit?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) 
         	sync();
        System.exit(0);
     }
 
-    private class syncListener implements ActionListener  { public void actionPerformed(ActionEvent e)  { sync(); } }
+    private class syncListener implements ActionListener  { public void actionPerformed(ActionEvent e)  { try {
+		sync();
+	} catch (Exception e1) {
+		e1.printStackTrace();
+	} } }
     
-    private class loadListener implements ActionListener  {  public void actionPerformed(ActionEvent e) { load(); } }
+    private class loadListener implements ActionListener  {  public void actionPerformed(ActionEvent e) { try {
+		load();
+	} catch (Exception e1) {
+		e1.printStackTrace();
+	} } }
     
     public static void main(String[] args) { new CloudNotesGUI(); }
 
